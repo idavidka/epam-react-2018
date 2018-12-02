7fb9ab61b45e075ad3d5ee562154c9cacabb7525
Imre Dávid <envagyok@idavid.hu>
2018-12-02 10:49:26 +0100

    Configured tests

--- tests/app.js
+++
@@ -1,1 +0,0 @@
-console.log('TODO: Create test files');
---
+++ tests/app.test.js
@@ -0,0 +1,5 @@
+import Search from '../lib/search';
+
+test('search should be a function', () => {
+    expect(Search).toBeInstanceOf(Function);
+});
